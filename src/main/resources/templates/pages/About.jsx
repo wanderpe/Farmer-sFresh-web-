840d49fda1982ec3f60ef7b4ccb980571a497318
anfheirne3 <132910616+anfheirne3@users.noreply.github.com>
2023-11-25 13:06:49 +0800

    Add files via upload

---
+++ src/main/resources/templates/pages/About.jsx
@@ -0,0 +1,150 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './About.css'
+import './AboutIndex.css'
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <img
+        className="pexels-rafa-de-3013440-1-icon"
+        alt=""
+        src="src/image/pexels-rafa-de-3013440 1.png"
+      />
+      <img
+        className="logo-farmers-fresh-1"
+        alt=""
+        src="src/image/Logo Farmers Fresh 1.svg"
+      />
+      <div className="about-us-child" />
+      <b className="farmersfreshgmailcom">farmersfresh@gmail.com</b>
+      <b className="talisaycity-cebu-ph">Talisay,City Cebu PH</b>
+      <img className="pin-alt-icon1" alt="" src="src/image/Pin_alt.svg" />
+      <img className="social-icons" alt="" src="src/image/Social Icons.svg" />
+      <img className="social-icons1" alt="" src="src/image/Social Icons 2.svg" />
+      <img className="social-icons2" alt="" src="src/image/Social Icons 3.svg" />
+      <img className="social-icons3" alt="" src="src/image/Social Icons 4.svg" />
+      <div className="message-alt-fill-parent">
+        <div className="message-alt-fill" />
+        <div className="group-child" />
+        <b className="home">Home</b>
+        <b className="about">About</b>
+        <b className="services">Services</b>
+        <b className="shop">Shop</b>
+        <b className="contact">Contact</b>
+      </div>
+      <div className="rectangle-parent">
+        <div className="group-item" />
+        <button className="search1">
+          <div className="search-inner" />
+          <img className="vector-icon" alt="bogo" src="src/image/Search.svg" />
+        </button>
+        <div className="cart-outline-wrapper">
+          <img className="cart-outline-icon1" alt="" src="src/image/Cart.svg" />
+        </div>
+      </div>
+      <div className="about-us-item" />
+      <div className="homeabout">Home/About</div>
+      <b className="about-us1">
+        <span className="us">About</span>
+        {` `}
+        <span className="us">Us</span>
+      </b>
+      <div className="about-us-inner" />
+      <img className="rectangle-icon" alt="" src="src/image/rectangle-8.png" />
+      <img className="about-us-child1" alt="" src="src/image/Rectangle 9.png" />
+      <img className="about-us-child2" alt="" src="src/image/Rectangle 10.png" />
+      <div className="rectangle-div" />
+      <div className="rectangle-div" />
+      <div className="rectangle-div" />
+      <b className="farmers-fresh-is-container">
+        <p className="farmers-fresh-is">
+          Farmer's Fresh is an e-commerce app that connects farmers with
+          consumers, allowing them to sell their fresh produce directly. We
+          believe that everyone deserves access to high-quality, affordable
+          food, and we're committed to making that happen.
+        </p>
+        <p className="farmers-fresh-is">&nbsp;</p>
+        <p className="farmers-fresh-is">
+          Our platform is easy to use for both farmers and consumers. Farmers
+          can create a profile and list their products for sale, and consumers
+          can browse the listings and purchase products directly from the
+          farmer. We handle all of the shipping and payment processing, so
+          farmers can focus on growing great produce and consumers can enjoy
+          fresh, local food without having to leave their homes.
+        </p>
+        <p className="farmers-fresh-is">&nbsp;</p>
+      </b>
+      <img
+        className="section-title-icon-1-1"
+        alt=""
+        src="src/image/section-title-icon-1 3.png"
+      />
+      <img
+        className="section-title-icon-1-2"
+        alt=""
+        src="src/image/section-title-icon-1 3.png"
+      />
+     
+      <div className="about-us-child5" />
+      <b className="b">85%</b>
+      <b className="b1">98%</b>
+      <div className="about-us-child6" />
+      <div className="about-us-child7" />
+      <div className="fresh-from-the-container">
+        <p className="farmers-fresh-is">Fresh from the farm</p>
+      </div>
+      <div className="organic">Organic</div>
+      <b className="our-commitment-to">Our Commitment to Quality</b>
+      <div className="we-believe-that-container">
+        <span className="we-believe-that-container1">
+          <p className="farmers-fresh-is">
+            We believe that everyone deserves access to the freshest, most
+            delicious food possible. That's why we're committed to securing the
+            best quality produce from our partner farmers.
+          </p>
+          <p className="farmers-fresh-is">&nbsp;</p>
+          <p className="farmers-fresh-is">
+            So when you order from Farmer's Fresh, you can be confident that
+            you're getting the best quality produce available.
+          </p>
+        </span>
+      </div>
+      <div className="team-members-container">
+        <b className="team-members">Team Members</b>
+        <b className="meet-our-farmers">Meet Our Farmers</b>
+      </div>
+      <img className="subtract-icon" alt="" src="/subtract.svg" />
+      <img
+        className="pexels-rafa-de-3013440-2-icon"
+        alt=""
+        src="src/image/pexels-rafa-de-3013440 1.png"
+      />
+      <img className="about-us-child8" alt="" src="src/image/Rectangle 15.png" />
+      <img className="about-us-child9" alt="" src="src/image/Rectangle 16.png" />
+      <img className="about-us-child10" alt="" src="src/image/Rectangle 17.png" />
+      <img className="about-us-child11" alt="" src="src/image/Rectangle 18.png" />
+      <img className="about-us-child12" alt="" src="src/image/Rectangle 24.png" />
+      <div className="about-us-child13" />
+      <b className="harvy-pettes-ardiza">Harvy Pettes Ardiza</b>
+      <div className="ako-ay-pilipino">Ako ay pilipino iwagayway</div>
+      <div className="about-us-child14" />
+      <div className="about-us-child15" />
+      <div className="about-us-child15" />
+      <div className="about-us-child17" />
+      <div className="about-us-child18" />
+      <b className="donnell-keith-delgado">Donnell Keith Delgado</b>
+      <div className="hi-its-me">Hi its me</div>
+      <div className="rasta-man">Rasta man</div> 
+      <div className="ceo">CEO</div>
+      <div className="ceo1">CEO</div>
+      <b className="anfheirne-caizares">Anfheirne Cañizares</b>
+      <b className="ariel-bernal">Ariel Bernal</b>
+      <b className="loraine-rotor">Loraine Rotor</b>
+      <div className="frame-div" />
+      <div className="about-us-child19" />
+    </div>
+  );
+};
+
+export default AboutUs;
